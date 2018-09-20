098382310c3e2ba6e99663faf348bd526fba9f4c
Kevin Bueno <kean.solano@gmail.com>
2018-09-20 16:38:03 -0500

    Rutas creadas
    Rutas listas

--- CaronteApp/src/app/app-routing/app-routing.module.ts
+++ CaronteApp/src/app/app-routing/app-routing.module.ts
@@ -1,24 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { HomeComponent } from './';
-import { Name2Component } from './';
-import { Name3Component } from './';
-import { Name4Component } from './';
-import { PageNotFoundComponent } from './';
+import { HomeComponent } from '../components/home/home.component';
+import { SearchComponent } from '../components/search/search.component';
+import { RegisterComponent } from '../components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'path2', component: Name2Component },
-  { path: 'path3', component: Name3Component },
-  { path: 'path4', component: Name4Component },
-  { path: '**', component: PageNotFoundComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: HomeComponent }
 
-  //{ path: 'path/:routeParam', component: MyComponent },
-  //{ path: 'staticPath', component: ... },
-  //{ path: '**', component: ... },
-  //{ path: 'oldPath', redirectTo: '/staticPath' },
-  //{ path: ..., component: ..., data: { message: 'Custom' }
 ];
 
 @NgModule({
--- CaronteApp/src/app/app.module.ts
+++ CaronteApp/src/app/app.module.ts
@@ -2,21 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
+// Router
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    TranslateModule.forRoot()
+    TranslateModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

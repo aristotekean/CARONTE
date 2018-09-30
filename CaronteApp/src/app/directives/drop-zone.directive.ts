90f0879d3732f66f20c1ed9ca7fabcceb18a49be
Kevin Bueno <kean.solano@gmail.com>
2018-09-30 10:49:27 -0500

    Drop zone

---
+++ CaronteApp/src/app/directives/drop-zone.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, HostListener, HostBinding, Output, EventEmitter } from '@angular/core';
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[dropZone]'
+})
+export class DropZoneDirective {
+
+  @Output() dropped = new EventEmitter<FileList>();
+  @Output() hovered = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  @HostListener('drop', ['$event'])
+  onDrop($event) {
+    $event.preventDefault();
+    this.dropped.emit($event.dataTransfer.files);
+    this.hovered.emit(false);
+  }
+
+  @HostListener('dragover', ['$event'])
+  ondragover($event) {
+    $event.preventDefault();
+    this.hovered.emit(true);
+  }
+
+  @HostListener('dragleave', ['$event'])
+  ondragleave($event) {
+    $event.preventDefault();
+    this.hovered.emit(false);
+  }
+
+}

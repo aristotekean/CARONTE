3913d9beedc8b0d7767c67a2c3a1d62e3dc08a87
Kevin Bueno <kean.solano@gmail.com>
2018-09-30 10:49:27 -0500

    Drop zone

--- CaronteApp/src/app/app.module.ts
+++ CaronteApp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
 // Router
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -12,11 +11,15 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 // Services
 import { CrudService } from './services/crud.service';
 import { FaceApiService } from './services/face-api.service';
+import { UploadsService } from './services/uploads.service';
 
+// Directives
+import { DropZoneDirective } from './directives/drop-zone.directive';
 
 // Componentes
 import { AppComponent } from './app.component';
@@ -26,6 +29,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { SearchComponent } from './components/search/search.component';
 import { RegisterComponent } from './components/register/register.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { FileSizePipe } from './pipes/file-size.pipe';
+
 
 @NgModule({
   declarations: [
@@ -35,7 +40,9 @@ import { FileUploadComponent } from './components/file-upload/file-upload.compon
     FooterComponent,
     SearchComponent,
     RegisterComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    DropZoneDirective,
+    FileSizePipe
   ],
   imports: [
     BrowserModule,
@@ -44,11 +51,13 @@ import { FileUploadComponent } from './components/file-upload/file-upload.compon
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule.enablePersistence(),
     AppRoutingModule,
+    AngularFireStorageModule,
     HttpModule
   ],
   providers: [
     CrudService,
-    FaceApiService
+    FaceApiService,
+    UploadsService
   ],
   bootstrap: [AppComponent]
 })
